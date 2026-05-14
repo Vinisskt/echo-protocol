3de8d46c4f4179d6c8c8fd22f1f0889ce929c099
vinisskt <vinissktmaloca@gmail.com>
2026-05-14 12:38:37 -0300

    Feat: Implement get_bits function and add explanatory comments

--- src/rb_bits.c
+++ src/rb_bits.c
@@ -8,18 +8,21 @@ Buffer* rb_init() {
 	buf->buf[buf->head] = 0;
 	buf->head = 0;
 	buf->tail = 0;
-	buf->count = 0;
+	buf->count_put = 0;
+	buf->count_get = 0;
 	return buf;
 }
 
 uint8_t check_rb(Buffer *buf) {
 	if (buf == NULL) {
-		printf("[Error] memory alocated");
+		printf("[Error] allocating memory");
 		return 1;
 	}
 	return 0;
 }
 
+
+// insere bits no rb -> inserindo bit a bit em um byte;
 uint8_t put_bits(Buffer *buf, uint8_t *bit) {
 	
 	if ( ((buf->head + 1) & BUFFER_MASK) == buf->tail) {
@@ -27,14 +30,32 @@ uint8_t put_bits(Buffer *buf, uint8_t *bit) {
 		return 0;
 	}
 	
-	if (buf->count == 8) {
-		buf->head++;
+	if (buf->count_put == 8) {
+		buf->head = (buf->head + 1) & BUFFER_MASK;
 		buf->buf[buf->head] = 0;
-		buf->count = 0;
+		buf->count_put = 0;
 	}
 
 	buf->buf[buf->head] = (buf->buf[buf->head] << 1) | *bit;
-	buf->count++;
+	buf->count_put++;
 
+	return 1;
+}
+
+// consome bit do rb -> pega os byte do rb e insere bit a bit no parametro bit;
+uint8_t get_bits(Buffer *buf, uint8_t *bit) {
+
+	if (buf->count_get == 8) {
+		buf->tail = (buf->tail + 1) & BUFFER_MASK;
+		buf->count_get = 0;
+	}
+
+	if ( buf->tail == buf->head && buf->count_get == buf->count_put ) {
+		// buffer empty
+		return 0;
+	}
+
+	*bit = (buf->buf[buf->tail] >> (7 - buf->count_get)) & 1;
+	buf->count_get++;
 	return 1;
 }
