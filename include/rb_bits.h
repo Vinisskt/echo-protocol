4a072d95c29db4f7007c1e92e8547fc5870bc64c
vinisskt <vinissktmaloca@gmail.com>
2026-05-14 12:33:21 -0300

    Chore: Update bit buffer header

--- include/rb_bits.h
+++ include/rb_bits.h
@@ -7,9 +7,11 @@ typedef struct {
 	uint8_t buf[BUFFER_SIZE];
 	uint16_t head;
 	uint16_t tail;
-	uint8_t count;
+	uint8_t count_put;
+	uint8_t count_get;
 } Buffer;
 
 Buffer* rb_init();
 uint8_t check_rb(Buffer *buf);
 uint8_t put_bits(Buffer *buf, uint8_t *bit);
+uint8_t get_bits(Buffer *buf, uint8_t *bit);
