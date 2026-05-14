cbe4acdcd215e5e96dbc4153b2fbf203992940ec
vinisskt <vinissktmaloca@gmail.com>
2026-05-14 02:45:21 -0300

    feat: add bit-level ring buffer header

---
+++ include/rb_bits.h
@@ -0,0 +1,15 @@
+#include <stdint.h>
+
+#define BUFFER_SIZE 1024
+#define BUFFER_MASK 1023
+
+typedef struct {
+	uint8_t buf[BUFFER_SIZE];
+	uint16_t head;
+	uint16_t tail;
+	uint8_t count;
+} Buffer;
+
+Buffer* rb_init();
+uint8_t check_rb(Buffer *buf);
+uint8_t put_bits(Buffer *buf, uint8_t *bit);
