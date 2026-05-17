3a9b6456c329cfb3313cac99302e6c3fa21c8f07
vinisskt <vinissktmaloca@gmail.com>
2026-05-17 01:02:03 -0300

    Feat: Add AFSK modulation header definitions

---
+++ include/mod_afsk.h
@@ -0,0 +1,24 @@
+#include "../include/rb_bits.h"
+#include <stdint.h>
+
+#define FREQ_SPACE 1200
+#define FREQ_MARK 2400
+#define SAMPLE_RATE 48000
+#define SYNC_WORD 0x930B51DE
+#define BIT_RATE 1200
+#define SAMPLES_PER_BIT (SAMPLE_RATE / BIT_RATE)
+#define PREAMBLE 1010101010101010
+
+typedef struct {
+	float step_cos_space;
+	float step_cos_mark;
+	float step_sin_space;
+	float step_sin_mark;
+	float current_sin;
+	float current_cos;
+	uint16_t sample_count;
+} StateAFSK;
+
+void pre_calc_afsk(StateAFSK *state);
+void generate_afsk(StateAFSK *state, uint8_t *bit);
+
