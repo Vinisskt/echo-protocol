c508697a2671ac7ce8d608c4157fd368e59047cf
vinisskt <vinissktmaloca@gmail.com>
2026-05-18 03:08:35 -0300

    Feat: Define AFSK demodulation and Goertzel structures

---
+++ include/demod_afsk.h
@@ -0,0 +1,13 @@
+#include "../include/rb_bits.h"
+#include <stdint.h>
+
+typedef struct {
+	int n;
+	float k, omega, coeff, q1, q2; 
+} StateGoertzel;
+
+uint8_t check_sync_word(uint32_t *check_word, uint8_t *bit);
+void pre_calc_goertzel(StateGoertzel *state, uint16_t *freq);
+float process_goertzel(StateGoertzel *state, float *sample);
+
+
