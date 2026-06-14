d47f628d8d0f5173399bc302206012edec3f6c4f
vinisskt <vinissktmaloca@gmail.com>
2026-06-14 02:03:48 -0300

    include: refactor EchoProtocol header for atomic packet ready flag

--- include/echo_protocol.h
+++ include/echo_protocol.h
@@ -5,6 +5,7 @@
 #include "rb_bits.h"
 #include "mod_afsk.h"
 #include "demod_afsk.h"
+#include <stdatomic.h>
 
 #define SIZE_BUF 2048
 #define SIZE_BYTE 8
@@ -15,15 +16,22 @@ typedef enum {
 } RxStatus;
 
 typedef struct {
+    uint16_t tx_sample_count;
+} TxState;
+
+typedef struct {
     RxStatus state;
     uint32_t sync_accumulator;
     uint16_t rx_sample_count;
     uint16_t bits_received;
     uint16_t packet_len;
     uint64_t header_accumulator;
+    uint32_t last_rx_time;
+    atomic_uint_fast8_t packet_ready;
+    uint8_t is_compressed;
 } RxState;
 
-typedef struct {
+typedef struct EchoProtocol_s {
     int tun_fd;
     Buffer *tx_rb;
     Buffer *rx_rb;
@@ -31,6 +39,7 @@ typedef struct {
     StateGoertzel space_state;
     StateGoertzel mark_state;
     RxState rx;
+    TxState tx;
 } EchoProtocol;
 
 int echo_init(EchoProtocol *echo, char *dev_name);
