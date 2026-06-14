315fcbc94d22b77773bf967d3ccebd9eea6e5084
vinisskt <vinissktmaloca@gmail.com>
2026-06-14 02:04:14 -0300

    include: add interface configuration prototypes

--- include/tun_tap.h
+++ include/tun_tap.h
@@ -7,4 +7,8 @@ int tun_alloc(char *dev, int flags);
 int tun_read(int fd, uint8_t *buf, uint16_t len);
 int tun_write(int fd, uint8_t *buf, uint16_t len);
 
+int tun_set_ip(const char *dev, const char *ip);
+int tun_set_mtu(const char *dev, int mtu);
+int tun_set_up(const char *dev);
+
 #endif // TUN_TAP_H
